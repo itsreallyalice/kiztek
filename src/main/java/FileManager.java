5de973e70d7817de79b290faae949ee048abf6cc
itsreallyalice <ai286@sussex.ac.uk>
2024-06-11 16:03:36 +0100

    Builds with Maven now!

--- src/main/java/FileManager.java
+++ src/main/java/FileManager.java
@@ -1,20 +1,18 @@
 import java.io.*;
 import java.util.Properties;
+import java.util.ResourceBundle;
 
 public class FileManager {
-
-    private static final String PROPERTIES_FILE = "config.properties";
+    private static final String PROPERTIES_FILE_BASE_NAME = "config";
     private static final String LAST_OPENED_FILE_KEY = "lastOpenedFile";
+    private static ResourceBundle resourceBundle = ResourceBundle.getBundle(PROPERTIES_FILE_BASE_NAME);
+
+
+
+
 
     public static String getLastOpenedFilePath() {
-        Properties properties = new Properties();
-        try (InputStream input = new FileInputStream(PROPERTIES_FILE)) {
-            properties.load(input);
-            return properties.getProperty(LAST_OPENED_FILE_KEY);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return null;
-        }
+        return resourceBundle.getString(LAST_OPENED_FILE_KEY);
     }
 
     public static String readFileToString(File file) throws IOException {
@@ -29,10 +27,20 @@ public class FileManager {
     }
     public static void saveLastOpenedFilePath(String path) {
         Properties properties = new Properties();
-        properties.setProperty(LAST_OPENED_FILE_KEY, path);
-        try (OutputStream output = new FileOutputStream(PROPERTIES_FILE)) {
-            properties.store(output, null);
+        // Load existing properties
+        try (InputStream input = FileManager.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE_BASE_NAME + ".properties")) {
+            if (input != null) {
+                properties.load(input);
+            }
         } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        // Set the new property
+        properties.setProperty(LAST_OPENED_FILE_KEY, path);
+        // Save properties back to the file
+        try (OutputStream output = new FileOutputStream(new File(FileManager.class.getClassLoader().getResource(PROPERTIES_FILE_BASE_NAME + ".properties").toURI()))) {
+            properties.store(output, null);
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
