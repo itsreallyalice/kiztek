ecb3a40ee9218ec714a8e5b779dd954a6862771a
itsreallyalice <ai286@sussex.ac.uk>
2024-06-22 23:41:38 +0100

    Sidebar works, add upload image and new file functionality

--- src/main/java/PDFCompiler.java
+++ src/main/java/PDFCompiler.java
@@ -8,8 +8,56 @@ public class PDFCompiler {
             tempFile.deleteOnExit();
 //            Files.write(tempFile.toPath(), latexDocument.getBytes());
 
+            ProcessBuilder latex1ProcessBuilder = new ProcessBuilder("pdflatex", latexDocument.getName()).directory(latexDocument.getParentFile());
+            latex1ProcessBuilder.redirectErrorStream(true);
+            Process latex1Process = latex1ProcessBuilder.start();
+            try (InputStream inputStream = latex1Process.getInputStream();
+                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    System.out.println(line);
+                }
+            }
+            int latex1ExitCode = latex1Process.waitFor();
+            System.out.println("latex[1] exited with code: " + latex1ExitCode);
+
+            //CHANGE SO NO .TEX EXTENSION
+            ProcessBuilder bibtexProcessBuilder = new ProcessBuilder("bibtex", latexDocument.getName()).directory(latexDocument.getParentFile());
+            bibtexProcessBuilder.redirectErrorStream(true);
+            Process bibtexProcess = bibtexProcessBuilder.start();
+            try (InputStream inputStream = bibtexProcess.getInputStream();
+                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    System.out.println(line);
+                }
+            }
+            int bibtexExitCode = bibtexProcess.waitFor();
+            System.out.println("bibtex exited with code: " + bibtexExitCode);
+
+            ProcessBuilder latex2ProcessBuilder = new ProcessBuilder("pdflatex", latexDocument.getName()).directory(latexDocument.getParentFile());
+            latex2ProcessBuilder.redirectErrorStream(true);
+            Process latex2Process = latex2ProcessBuilder.start();
+            try (InputStream inputStream = latex2Process.getInputStream();
+                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    System.out.println(line);
+                }
+            }
+            int latex2ExitCode = latex2Process.waitFor();
+            System.out.println("latex[1] exited with code: " + latex2ExitCode);
+
             ProcessBuilder processBuilder = new ProcessBuilder("pdflatex", "-output-directory=" + tempFile.getParent(), latexDocument.getName())
                     .directory(latexDocument.getParentFile());
+            //run bibtex as well
+            //run this on a different thread so no freezing.
             processBuilder.redirectErrorStream(true);
             Process process = processBuilder.start();
 
